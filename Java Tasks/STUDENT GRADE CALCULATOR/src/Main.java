700290b7d42de4ba043bcf8e99b94e3b6b95d660
3ab2wy1911 <3ab2wy1911@gmail.com>
2023-09-08 16:30:43 +0300

    Task 2
    TASK 2
    STUDENT GRADE CALCULATOR

--- Java Tasks/STUDENT GRADE CALCULATOR/src/Main.java
+++ Java Tasks/STUDENT GRADE CALCULATOR/src/Main.java
@@ -12,77 +12,68 @@ Show the total marks, average percentage, and the corresponding grade to the use
 
 import java.util.Scanner;
 
-class calculator{
+class calculator {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void calculate () {
+    public static void calculate() {
         System.out.println("Welcome to Student Grade Calculator :)");
         System.out.println("****************************************************************");
         System.out.println("Please enter the number of subjects :");
         int subjectsNumber = scanner.nextInt();
         float marks;
         float total = 0.0f;
-        int i =0;
+        int i = 0;
         String grade;
         //______________________________________________________________________________________________________________
-        while (i++!=subjectsNumber) {   // Getting the marks from the user.
+        while (i++ != subjectsNumber) {   // Getting the marks from the user.
             System.out.println("Enter the marks of the subject out of 100 :");
             marks = scanner.nextFloat();
             //__________________________________________________________________________________________________________
             // Validate the marks.
-            while (marks <0 || marks > 100) {
+            while (marks < 0 || marks > 100) {
                 System.out.println("Invalid Mark !!!");
                 marks = scanner.nextFloat();
             }
             //___________________________________________________________________________________________________________
             total += marks; // getting the total marks of all subjects.
-            System.out.println("Remaining Subjects: " + (subjectsNumber-i)); // Display the number of remaining subjects.
+            System.out.println("Remaining Subjects: " + (subjectsNumber - i)); // Display the number of remaining subjects.
         }
         //______________________________________________________________________________________________________________
         float avgPercentage = total / subjectsNumber;   // Calculate Average Percentage.
         //______________________________________________________________________________________________________________
         //Grade Calculation.
-        if (avgPercentage>=90){
+        if (avgPercentage >= 90) {
             grade = "A+";   // Omar Mohamed & Mohamed Amir favourite grade.
-        }
-        else if (avgPercentage >= 85){
+        } else if (avgPercentage >= 85) {
             grade = "A";
-        }
-        else if (avgPercentage >= 80){
+        } else if (avgPercentage >= 80) {
             grade = "B+";
-        }
-        else if (avgPercentage >= 75){
+        } else if (avgPercentage >= 75) {
             grade = "B";
-        }
-        else if (avgPercentage >= 70){
+        } else if (avgPercentage >= 70) {
             grade = "C+";
-        }
-        else if (avgPercentage >= 65){
+        } else if (avgPercentage >= 65) {
             grade = "C";
-        }
-        else if (avgPercentage >=60){
+        } else if (avgPercentage >= 60) {
             grade = "D+";
-        }
-        else if(avgPercentage >= 50){
+        } else if (avgPercentage >= 50) {
             grade = "D";
-        }
-        else {
+        } else {
             grade = "F";
         }
         //______________________________________________________________________________________________________________
         // Display Results.
         System.out.print(
                 """
-                       |+++++++|++++++++++++++++++++|+++++++|
-                       | Total | Average Percentage | Grade |
-                       |+++++++|++++++++++++++++++++|+++++++|
-                       """
+                        +-------+----------------------+-------+
+                        | Total | Average Percentage | Grade |
+                        +-------+----------------------+-------+
+                        """
         );
-        System.out.println("| " + total + " | " + avgPercentage+" | "+grade+" |");
-        System.out.println("|+++++++|++++++++++++++++++++|+++++++|");
+        System.out.printf("| %-5s | %10s %10s| %-5s |\n", total, avgPercentage, "",grade);  // Format the output.
+        System.out.println("+-------+----------------------+-------+");
     }
 }
-
 //______________________________________________________________________________________________________________________
 public class Main {
     public static void main(String[] args) {
